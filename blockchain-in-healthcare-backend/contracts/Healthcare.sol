518e879348cd79f8ec571238a59f701d4aeffda9
reuben21 <reuben211999@icloud.com>
2022-01-06 19:42:06 +0530

    updated code

--- blockchain-in-healthcare-backend/contracts/Healthcare.sol
+++ blockchain-in-healthcare-backend/contracts/Healthcare.sol
@@ -1,6 +1,7 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//SPDX-License-Identifier: MIT
+
 
 import "./access/AccessControlEnumerable.sol";
 import "./Roles.sol";
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-var HospitalContract = artifacts.require("./Hospital.sol");
-var PatientContract = artifacts.require("./Patient.sol");
-var DoctorContract = artifacts.require("./Doctor.sol");
-var PharmacyContract = artifacts.require("./Pharmacy.sol");
-var HealthcareContract = artifacts.require("./Healthcare.sol");
+// var HospitalContract = artifacts.require("./Hospital.sol");
+// var PatientContract = artifacts.require("./Patient.sol");
+// var DoctorContract = artifacts.require("./Doctor.sol");
+// var PharmacyContract = artifacts.require("./Pharmacy.sol");
+// var HealthcareContract = artifacts.require("./Healthcare.sol");
+var MainContract = artifacts.require("./MainContract.sol");
 // var Roles = artifacts.require("./Roles.sol");
 
 var HospitalCoinContract = artifacts.require("./HospitalToken.sol");
@@ -11,10 +12,10 @@ module.exports = function (deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(Roles);
-  deployer.deploy(HospitalContract);
-  deployer.deploy(PatientContract);
-  deployer.deploy(DoctorContract);
-  deployer.deploy(PharmacyContract);
+  // deployer.deploy(HospitalContract);
+  // deployer.deploy(PatientContract);
+  // deployer.deploy(DoctorContract);
+  // deployer.deploy(PharmacyContract);
 
   // deployer.link(Roles, PatientContract);
   // deployer.link(Roles, HospitalContract);
@@ -23,14 +24,14 @@ module.exports = function (deployer) {
   // deployer.link(Roles, PharmacyContract);
   // deployer.link(Roles, HealthcareContract);
 
-  deployer.link(PatientContract, DoctorContract);
+  // deployer.link(PatientContract, DoctorContract);
 
-  deployer.link(HospitalContract,HealthcareContract);
-  deployer.link(PatientContract,HealthcareContract);
-  deployer.link(DoctorContract,HealthcareContract);
-  deployer.link(PharmacyContract,HealthcareContract);
+  // deployer.link(HospitalContract,HealthcareContract);
+  // deployer.link(PatientContract,HealthcareContract);
+  // deployer.link(DoctorContract,HealthcareContract);
+  // deployer.link(PharmacyContract,HealthcareContract);
   
-  deployer.deploy(HealthcareContract);
+  deployer.deploy(MainContract);
 
 
 
