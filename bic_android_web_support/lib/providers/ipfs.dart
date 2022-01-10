7fb1a53cba8602e5886b94b309b1e0bac1170a24
reuben21 <reuben211999@icloud.com>
2022-01-10 15:02:25 +0530

    Updated code

--- bic_android_web_support/lib/providers/ipfs.dart
+++ bic_android_web_support/lib/providers/ipfs.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
