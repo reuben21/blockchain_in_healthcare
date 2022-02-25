d4a4f4a6ccc7bc49b1ae1b30f1820a18fa3d266f
ankita-1812 <tripathiankita242@gmail.com>
2022-02-25 09:31:21 +0530

    updated url

--- bic_android_web_support/lib/helpers/keys.dart
+++ bic_android_web_support/lib/helpers/keys.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
@@ -17,16 +15,15 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 // }
 
 String get rpcUrl {
-  if(kIsWeb) {
-    return "http://192.168.0.100:7545";
-  }
-  else if(Platform.isWindows) {
-    return "http://192.168.0.100:7545";
+  if (kIsWeb) {
+    return "HTTP://192.168.137.1:7545";
+  } else if (Platform.isWindows) {
+    return "HTTP://192.168.137.1:7545";
   } else if (Platform.isAndroid) {
-    return  "http://192.168.0.100:7545";
+    return "HTTP://192.168.137.1:7545";
   }
 
-  return "http://192.168.0.100:7545";
+  return "HTTP://192.168.137.1:7545";
 }
 
 // String get rpcUrl {
@@ -43,37 +40,33 @@ String get rpcUrl {
 // }
 
 String get rpcUrlWebSocket {
-  if(kIsWeb) {
+  if (kIsWeb) {
     return "ws://127.0.0.1:7545";
-  }
-  else if(Platform.isWindows) {
+  } else if (Platform.isWindows) {
     return "ws://127.0.0.1:7545";
   } else if (Platform.isAndroid) {
-    return  "ws://10.0.2.2:7545";
+    return "ws://10.0.2.2:7545";
   }
 
   return "http://127.0.0.1:7545";
 }
 
-
 String get getIpfsUrl {
-  if(kIsWeb) {
+  if (kIsWeb) {
     return "https://ipfs.infura.io:5001";
-  }
-  else if(Platform.isWindows) {
+  } else if (Platform.isWindows) {
     return "https://ipfs.infura.io:5001";
   } else if (Platform.isAndroid) {
-    return  "https://ipfs.infura.io:5001";
+    return "https://ipfs.infura.io:5001";
   }
 
   return "https://ipfs.infura.io:5001";
 }
 
 String get getIpfsUrlForReceivingData {
-  if(kIsWeb) {
+  if (kIsWeb) {
     return "https://ipfs.infura.io:5001/api/v0/cat?arg=";
-  }
-  else if(Platform.isWindows) {
+  } else if (Platform.isWindows) {
     return "https://ipfs.infura.io:5001/api/v0/cat?arg=";
   } else if (Platform.isAndroid) {
     return "https://ipfs.infura.io:5001/api/v0/cat?arg=";
@@ -82,8 +75,8 @@ String get getIpfsUrlForReceivingData {
   return "https://ipfs.infura.io:5001/api/v0/cat?arg=";
 }
 
-
-String databaseUrl = "mongodb+srv://group22:310TXL42RKB0WW7v@mongodb.syifj.mongodb.net/healthcare?retryWrites=true&w=majority";
-const String privateKey = "0e75aade5bd385616574bd6252b0d810f3f03f013dc43cbe15dc2e21e6ff4f14";
+String databaseUrl =
+    "mongodb+srv://group22:310TXL42RKB0WW7v@mongodb.syifj.mongodb.net/healthcare?retryWrites=true&w=majority";
+const String privateKey =
+    "0e75aade5bd385616574bd6252b0d810f3f03f013dc43cbe15dc2e21e6ff4f14";
 const String publicKey = "0xC30aC339bd3479eb62eD6fE71C1A0A59FA177F7B";
-
